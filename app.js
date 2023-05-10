a4a0e2c0f56d1cce0743a89d03065d642e5815a0
leopzls <llopezle5480@universidadean.edu.co>
2023-05-10 02:50:42 -0500

    Adding the project until what I have

---
+++ app.js
@@ -0,0 +1,33 @@
+const { log } = require('console');
+const express = require ('express')
+
+const app = express ();
+
+const path = require('path')
+app.use(express.static('public'));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/index.html'))
+})
+
+app.get("/register", (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/register.html'))
+})
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/login.html'))
+})
+
+app.get("/productdetail", (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/productdetail.html'))
+})
+
+app.get("/shoppingcart", (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/shoppingcart.html'))
+})
+
+
+app.listen(3080, () => {
+    console.log('Servidor escuchando en el puerto 3080')
+});
+
